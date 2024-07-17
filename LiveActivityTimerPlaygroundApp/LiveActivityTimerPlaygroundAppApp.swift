d5fb90f6fba79411ef991f1626da76596c6afe6d
Daniel Yankiver <dy8950@att.com>
2024-07-17 11:11:11 -0400

    Initial Commit

---
+++ LiveActivityTimerPlaygroundApp/LiveActivityTimerPlaygroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityTimerPlaygroundAppApp.swift
+//  LiveActivityTimerPlaygroundApp
+//
+//  Created by dy8950 on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityTimerPlaygroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
