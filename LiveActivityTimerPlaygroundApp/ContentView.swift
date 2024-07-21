199495f8ce81797eb09101f6b8106be1b9d9c12c
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2024-07-21 10:48:19 -0400

    update 3

--- LiveActivityTimerPlaygroundApp/ContentView.swift
+++ LiveActivityTimerPlaygroundApp/ContentView.swift
@@ -25,7 +25,6 @@ struct ContentView: View {
   var body: some View {
     VStack {
       Spacer()
-
       if isTimerRunning {
         CircleTimerView(progress: $progress, duration: $duration)
       }
